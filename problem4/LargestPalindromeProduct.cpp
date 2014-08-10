f397f5e5f34b1a9c09ed553066df22db2335a905
gamza <gamzzaa@gmail.com>
2014-08-10 23:06:55 +0200

    initial file for largerst palindrome product problem

---
+++ problem4/LargestPalindromeProduct.cpp
@@ -0,0 +1,16 @@
+// Project Euler
+// -------------
+//
+// Problem 4. Largest palindrome product
+//
+// A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+// Find the largest palindrome made from the product of two 3-digit numbers.
+//
+// File: LargestPalindromeProduct.cpp
+
+#include <iostream>
+
+int main() {
+
+  return 0;
+}
